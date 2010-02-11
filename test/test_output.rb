dfa8b889165136ae512fc09c837537219a668147
blahed <tdunn13@gmail.com>
2010-02-11 03:05:58 -0500

    output tests now mirror render tests

--- test/test_output.rb
+++ test/test_output.rb
@@ -20,6 +20,42 @@ class TestBase < Test::Unit::TestCase
       assert File.exist? 'template/output'
     end
     
+    should 'create index.html' do
+      assert_equal "<h1>hello worlds</h1>\n\n", IO.read('template/output/index.html')
+    end
+    
+    should  'create partial_test.html' do
+      assert_equal "<h1>hello worlds</h1>\n<p>hello from partial</p>\n", IO.read('template/output/partial_test.html')   
+    end
+    
+    should 'create erb.html' do
+      assert_equal "<h1>hello worlds</h1>\n", IO.read('template/output/erb.html')
+    end
+    
+    should 'create redcloth.html' do
+      assert_equal "<h1>hello worlds</h1>", IO.read('template/output/redcloth.html')
+    end
+    
+    should 'create markdown.html' do
+      assert_equal "<h1>hello worlds</h1>\n", IO.read('template/output/markdown.html')
+    end
+    
+    should 'create mustache.html' do
+      assert_equal "<h1>hello worlds</h1>\n", IO.read('template/output/mustache.html')
+    end
+    
+    should 'create liquid.html' do
+      assert_equal "<h1>hello worlds</h1>", IO.read('template/output/liquid.html')
+    end
+    
+    should 'create builder.html' do
+      assert_equal "<h1>hello worlds</h1>\n", IO.read('template/output/builder.html')
+    end
+    
+    should 'copy static.html' do
+      assert_equal "hello from static", IO.read('template/output/static.html')
+    end
+    
     teardown do
       FileUtils.rm_r File.join(Dir.pwd, 'template/output')
     end
--- test/test_render.rb
+++ test/test_render.rb
@@ -26,43 +26,43 @@ class TestBase < Test::Unit::TestCase
     end
     
     should 'render sass template' do
-        template = @frank.render_path('sass.sass')
-        assert_equal "#hello-worlds {\n  background: red; }\n", template
-      end
+      template = @frank.render_path('sass.sass')
+      assert_equal "#hello-worlds {\n  background: red; }\n", template
+    end
       
-      should 'render erb template' do
-        template = @frank.render_path('erb.erb')
-        assert_equal "<h1>hello worlds</h1>\n", template
-      end
+    should 'render erb template' do
+      template = @frank.render_path('erb.erb')
+      assert_equal "<h1>hello worlds</h1>\n", template
+    end
+  
+    should 'render redcloth template' do
+      template = @frank.render_path('redcloth.textile')
+      assert_equal "<h1>hello worlds</h1>", template
+    end
     
-      should 'render redcloth template' do
-        template = @frank.render_path('redcloth.textile')
-        assert_equal "<h1>hello worlds</h1>", template
-      end
-      
-      should 'render rdiscount template' do
-        template = @frank.render_path('markdown.md')
-        assert_equal "<h1>hello worlds</h1>\n", template
-      end
-      
-      should 'render mustache template' do
-        template = @frank.render_path('mustache.mustache')
-        assert_equal "<h1>hello worlds</h1>\n", template
-      end
-      
-      should 'render liquid template' do
-        template = @frank.render_path('liquid.liquid')
-        assert_equal "<h1>hello worlds</h1>", template
-      end
-      
-      should 'render builder template' do
-        template = @frank.render_path('builder.builder')
-        assert_equal "<h1>hello worlds</h1>\n", template
-      end
-      
-      should 'raise template error' do
-        assert_raise(Frank::TemplateError) { @frank.render_path('not_a.template') }
-      end
+    should 'render rdiscount template' do
+      template = @frank.render_path('markdown.md')
+      assert_equal "<h1>hello worlds</h1>\n", template
+    end
+    
+    should 'render mustache template' do
+      template = @frank.render_path('mustache.mustache')
+      assert_equal "<h1>hello worlds</h1>\n", template
+    end
+    
+    should 'render liquid template' do
+      template = @frank.render_path('liquid.liquid')
+      assert_equal "<h1>hello worlds</h1>", template
+    end
+    
+    should 'render builder template' do
+      template = @frank.render_path('builder.builder')
+      assert_equal "<h1>hello worlds</h1>\n", template
+    end
+    
+    should 'raise template error' do
+      assert_raise(Frank::TemplateError) { @frank.render_path('not_a.template') }
+    end
       
   end
   

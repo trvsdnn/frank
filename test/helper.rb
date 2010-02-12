013fc7f2ab754d8b9a0a2fe8f09d2d937490b667
blahed <tdunn13@gmail.com>
2010-02-11 22:29:58 -0500

    cleaning up tests

--- test/helper.rb
+++ test/helper.rb
@@ -7,21 +7,6 @@ require 'template/helpers'
 require File.join(File.dirname(__FILE__), '../lib/frank')
 
 class Test::Unit::TestCase
-  include Rack::Test::Methods
-
-  LIBDIR = File.join(File.dirname(File.dirname(__FILE__)), 'lib')
-  
-  def mock_app(&block)
-    @app = Frank.new(&block)
-  end
-  
-  def app
-    Rack::Lint.new(@app)
-  end
-  
-  def body
-    response.body.to_s
-  end
-  
+  include Rack::Test::Methods 
 
 end
--- test/test_base.rb
+++ test/test_base.rb
@@ -2,59 +2,46 @@ require File.dirname(__FILE__) + '/helper'
 
 class TestBase < Test::Unit::TestCase
   
+  def app
+    settings = YAML.load_file('template/settings.yml')
+    Frank.new do
+      settings.each do |name, value|
+        set name.to_s, value
+      end
+      set :environment, :test
+      set :proj_dir, File.join(Dir.pwd, 'template')
+    end
+  end
+  
   context 'Frank::Base' do
     
-    setup do
-      settings = YAML.load_file('template/settings.yml')
-      @frank = Frank.new do
-        settings.each do |name, value|
-          set name.to_s, value
-        end
-        set :environment, :test
-        set :proj_dir, File.join(Dir.pwd, 'template')
-      end
-    end
-    
     should 'have all required settings set' do
-      assert_not_nil @frank.proj_dir
-      assert_not_nil @frank.server
-      assert_not_nil @frank.static_folder
-      assert_not_nil @frank.dynamic_folder
-      assert_not_nil @frank.templates
-    end
-    
-    should 'respond to call' do
-      assert_respond_to @frank, :call
+      assert_not_nil app.proj_dir
+      assert_not_nil app.server
+      assert_not_nil app.static_folder
+      assert_not_nil app.dynamic_folder
+      assert_not_nil app.templates
     end
     
     should 'render a dynamic template given a request' do
-      request = Rack::MockRequest.new(@frank)
-      response = request.get('/', {'SERVER_NAME' => 'localhost', 'SERVER_PORT' => '3601'})
-      assert response.ok?
+      get '/'
       
-      assert_match 'hello worlds', response.body
+      assert last_response.ok?
+      assert_equal "<h1>hello worlds</h1>\n\n", last_response.body
     end
     
     should 'render 404 page if template not found' do
-       request = Rack::MockRequest.new(@frank)
-       response = request.get('/not_here', 
-                              { 'SERVER_NAME' => 'localhost', 
-                                'SERVER_PORT' => '3601', 
-                                'REQUEST_PATH' => '/not_here'} )
+      get '/not_here'
                               
-       assert !response.ok?                          
-       assert_match 'Not Found', response.body
+      assert !last_response.ok?                          
+      assert_match 'Not Found', last_response.body
     end
     
     should 'render 500 page for error' do
-        request = Rack::MockRequest.new(@frank)
-        response = request.get('/?blah=broken',
-                               { 'SERVER_NAME' => 'localhost', 
-                                 'SERVER_PORT' => '3601', 
-                                 'REQUEST_PATH' => '/'})
-        
-        assert !response.ok?
-        assert_match "<tt>can't convert Fixnum into String</tt>", response.body
+      get '/?brok=en'
+      
+      assert !last_response.ok?
+      assert_match "undefined local variable or method `non_method'", last_response.body
     end
     
   end

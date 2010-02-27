d650d8ae122a0693bc0305dcedd20429cd7e5a24
blahed <tdunn13@gmail.com>
2010-02-26 22:43:01 -0500

    fix nasty bug when throwing path_info around

--- lib/frank/statik.rb
+++ lib/frank/statik.rb
@@ -13,18 +13,26 @@ module Frank
     # looks for static access, if not found,
     # passes request to frank
     def call(env)
-      path = env['PATH_INFO']
+      path = env['PATH_INFO'].dup
  
       if path.include? '__frank__'
         env['PATH_INFO'].gsub!('/__frank__', '')
         result = @frank_server.call(env)
-      elsif path.index('/') == 0
+      else
         env['PATH_INFO'] << '/' unless path.match(/(\.\w+|\/)$/)
         env['PATH_INFO'] << 'index.html' if path[-1..-1] == '/'
         result = @static_server.call(env)
       end
-      return result if result[0] == 200
-      @app.call(env)
+      
+      # return if static assets found
+      # else reset the path and pass to frank
+      if result[0] == 200
+        result
+      else
+        env['PATH_INFO'] = path
+        @app.call(env)
+      end
+      
     end
  
   end

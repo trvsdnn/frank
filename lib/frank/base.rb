792375fde62560ff34231d6acbff5c73eb988ea6
Jacques Crocker <railsjedi@gmail.com>
2010-07-30 00:08:30 -0700

    Add in automatic compass support

--- lib/frank/base.rb
+++ lib/frank/base.rb
@@ -233,10 +233,31 @@ module Frank
   def self.bootstrap(new_root = nil)
     Frank.reset
     Frank.root = new_root if new_root
+
+    # try to pull in setup
     setup = File.join(Frank.root, 'setup.rb')
     if File.exists?(setup)
       load setup
     end
+
+    # setup compass
+    begin
+      require 'compass'
+
+      Compass.configuration do |config|
+        # project_path should be the directory to which the sass directory is relative.
+        # I think maybe this should be one more directory up from the configuration file.
+        # Please update this if it is or remove this message if it can stay the way it is.
+        config.project_path = Frank.root
+        config.sass_dir = File.join('dynamic', 'stylesheets')
+      end
+
+      # sass_engine_options returns a hash, you can merge it with other options.
+      Frank.sass_options = Compass.sass_engine_options
+    rescue LoadError
+      # ignore if compass is not there
+    end
+
   end
 
   # starts the server
